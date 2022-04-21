074e6560275e6c62fae5720c1f884cbb8e039b08
DrogariaPlus12 <101207428+DrogariaPlus12@users.noreply.github.com>
2022-04-21 11:44:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,77 @@
-const fs = require('fs')
-const puppeteer = require("puppeteer-extra")
-const pluginStealth = require("puppeteer-extra-plugin-stealth")
-const _ = require('lodash')
-const XLSX = require('xlsx')
+const puppeter = require('puppeteer'); 
 
-puppeteer.use(pluginStealth())
+async function start() {
 
-puppeteer.launch({ 
-    headless:false,
-    args: ['--start-maximized']
-  }).then( async browser => {  
-  const page = await browser.newPage()
-  
-  page.setDefaultNavigationTimeout(0); //Acessa a página sem esperar o timeout
-  await page.goto('https://parceirohypera.com.br');
-  
-  await page.waitForSelector('input[name="usr"]');
-  
-  await page.type('input[name="usr"]', 'LOGIN',{delay:100}); // Email
-  
-  await page.type('input[name="psw"]', 'SENHA', {delay:100}); // Senha
-  
-  await page.keyboard.press('Enter');  // Clica no botão de login
-  
-   await page.waitForNavigation();
-   
-   await page.goto('https://parceirohypera.com.br/ItensProdutoPedido');
-   
-  await page.click('#prosseguirPolitCookie');
-  // captcha google
-  
-  const fileData = []
+const browser = await puppeter.launch({headless: false} );
+const page = await browser.newPage();
+// Acessa o site
+page.setDefaultNavigationTimeout(0); //Acessa a página sem esperar o timeout
+await page.goto('https://www.parceirohypera.com.br/Campanha');
+await page.click('#prosseguirPolitCookie');
+await page.waitForSelector('input[name="usr"]');
 
-  async function getData() {
-    async function getComments(page, selector){ 
-        const comments = await page.$$eval(selector, links => links.map(link => link.innerText))
-        return comments
-    }   //Acessa o site
-    for (let clicou = 0; clicou < 59; clicou++) {
-        const comments = await getComments(page,'#DataTables_Table_0 > tbody');
-        await page.click('#DataTables_Table_0_next > a');
-        await page.waitForTimeout(500);
-        console.log(comments);
-        fileData.push(comments)    
-    }  //Acessa o site 
-    await browser.close()
-  }
-  await getData()
+await page.type('input[name="usr"]', '15986565000167',{delay:100}); // Email
 
-  const flattenFileData = _.flattenDeep(fileData)
-  const flattenAndSeparatedFileData = _.chunk(flattenFileData, 5)
+await page.type('input[name="psw"]', '', {delay:100}); // Senha
 
-  fs.writeFile(
-    './data.json',
-    JSON.stringify(flattenAndSeparatedFileData, null, 2),
-    function (err) {
-      if (err) throw err
-      console.log('Arquivo Gerado')
-    }
-  )
+await page.keyboard.press('Enter');  // Clica no botão de login
 
-  const workSheet = XLSX.utils.json_to_sheet(flattenAndSeparatedFileData)
-  const wb = XLSX.utils.book_new()
-  
-  XLSX.utils.book_append_sheet(wb, workSheet, 'info_extract')
-  XLSX.writeFile(wb, 'dados.xlsb')
-})
+
+await page.waitForNavigation();
+
+const rotina = () => {
+    let nome = await page.$$eval('div.row.col-12 > div.col-8 > div:nth-child(2) > div:nth-child(1) > div', links => links.map(link => link.innerText.trim()));
+    let codigo = await page.$$eval(' div.row.col-12 > div.col-8 > div:nth-child(2) > div:nth-child(2) > div', links => links.map(link => link.innerText.trim()));
+    let preco = await page.$$eval('div.row.col-12 > div.col-8 > div:nth-child(2) > div:nth-child(3) > div', links => links.map(link => link.innerText.trim()));
+    let desconto = await page.$$eval('div.row.col-12 > div.col-8 > div:nth-child(2) > div:nth-child(4) > div', links => links.map(link => link.innerText.trim()));
+    return rotina();
+}
+
+        await page.waitForTimeout('8000');
+
+            await page.evaluate(() => {
+                document.querySelector("#\\33 139 > div > a").click();
+            }).catch(error => { console.log(error) }); // Clica no botão de campanhas
+                
+                await page.waitForTimeout('5000');
+                rotina();
+                await page.evaluate(() => {
+                    document.querySelector("div.row.mt-5.mb-4 > div > a").click();
+                })   
+
+        await page.waitForTimeout('8000');
+
+        await page.evaluate(() => {
+            document.querySelector("#\\33 139 > div > a").click();
+        }).catch(error => { console.log(error) }); // Clica no botão de campanhas
+            
+            await page.waitForTimeout('5000');
+            rotina();
+            await page.evaluate(() => {
+                document.querySelector("div.row.mt-5.mb-4 > div > a").click();
+            })    
+
+const prints = [nome, codigo, preco, desconto]; 
+//tranformando em xlsx
+const xlsx = require('xlsx');
+//organizando o array
+const array = prints.map(item => [item]);
+//criando o arquivo
+const wb = xlsx.utils.book_new();
+//criando a planilha
+const ws = xlsx.utils.aoa_to_sheet(array);
+//adicionando a planilha ao arquivo
+xlsx.utils.book_append_sheet(wb, ws, 'Campanhas');
+//colocando data no nome do arquivo
+const date = new Date( Date.now() );
+//salvando o arquivo
+
+xlsx.writeFile(wb,'Promoções_'+date.getDate()+'_'+date.getMonth()+'_'+date.getFullYear()+'.xlsx');
+
+
+
+await browser.close();
+}
+    
+
+start()
